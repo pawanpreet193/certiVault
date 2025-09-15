a8810e581b391c77c8e01528515d1e2072426b0c
pawanpreet193 <pawanpreetsingh193@gmail.com>
2025-09-15 18:04:59 +0530

    cv_1: initial setup

---
+++ backend/certiVault/src/test/java/com/example/certiVault/certiVault/CertiVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.certiVault.certiVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertiVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
